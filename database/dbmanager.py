ff4f8516f57b9f0bd97f5bc1e3d7a7597a52ec83
Patrick <Patrick.dejong@protonmail.com>
2020-03-21 16:25:54 +0100

    Added more db entries and did more maintaince

--- database/dbmanager.py
+++ database/dbmanager.py
@@ -2,7 +2,6 @@ import sqlite3
 import paho.mqtt.client as mqttClient
 from sqlite3 import Error
 
-
 class dbManager:
 
 	def __init__(self, location):
@@ -21,42 +20,28 @@ class dbManager:
 		self.dbCreateTemperature = """CREATE TABLE IF NOT EXISTS iskra_temperature (
 								id integer PRIMARY KEY AUTOINCREMENT,
 								message_id TEXT NOT_NULL,
-								outside_temp REAL NOT_NULL,
-								inside_temp REAL NOT_NULL,
+								temperature REAL NOT_NULL,
 								signature TEXT NOT_NULL,
 								timestamp TEXT NOT_NULL
 							);"""
 
-		self.dbSmartmeter = """CREATE TABLE IF NOT EXISTS emon_smartmeter (
-							id integer PRIMARY KEY AUTOINCREMENT,
-							electricity_tariff TEXT,
-							energy_delivered_tariff1 REAL,
-							energy_delivered_tariff2 REAL,
-							energy_returned_tariff1 REAL,
-							energy_returned_tariff2 REAL,
-							equipment_id TEXT,
-							gas_device_type REAL,
-							gas_equipment_id TEXT,
-							identification TEXT,
-							p1_version TEXT,
-							power_delivered REAL,
-							power_delivered_l1 REAL,
-							power_returned REAL,
-							power_returned_l1 REAL,
-							timestamp TEXT,
-							signature TEXT
-						);"""
+		self.dbSimonMeter = """CREATE TABLE IF NOT EXISTS simon_meter (
+								id integer PRIMARY KEY AUTOINCREMENT,
+								energymeter_name TEXT NOT_NULL,
+								watt INTEGER NOT_NULL,
+								wH REAL NOT_NULL,
+								total INTEGER NOT_NULL,
+								signature TEXT NOT_NULL,
+								timestamp TEXT NOT_NULL
+							);"""
 
-			
 		self.overviewdb = """CREATE TABLE IF NOT EXISTS overview (
 							name TEXT,
 							table_name TEXT
 						);"""
+
+
 		self.conn = self.create_connection()
-
-
-
-
 
 	def create_connection(self):
 		conn = None
@@ -68,82 +53,16 @@ class dbManager:
 
 			return conn
 
-
 	def create_table(self):
 		try:
 			c = self.conn.cursor()
 			c.execute(self.dbCreate)
 			c.execute(self.dbCreateTemperature)
-			c.execute(self.dbSmartmeter)
+			c.execute(self.dbSimonMeter)
 			c.execute(self.overviewdb)
 			#self.insert_overview()
 		except Error as e:
 			print(e)
-
-	def insert_iskra_energie(self, message_id, energymeter_name, watt, wH, total, signature, timestamp):
-		sql = ''' INSERT INTO iskra_energie(message_id, energymeter_name, watt, wH, total, signature, timestamp)
-					VALUES(?,?,?,?,?,?,?)'''
-		try:
-			cur = self.conn.cursor()
-			result = cur.execute(sql, (str(message_id), str(energymeter_name), watt, wH, total,
-						  str(signature), str(timestamp)))
-			self.conn.commit()
-			result = self.conn.commit()
-			# return cur.lastrowid
-		except Error as e:
-			print(e)
-		except Exception as e:
-			print("Exception in _query: {e}")
-		
-
-	def insert_iskra_temperature(self, message_id, outside_temp, inside_temp, signature, timestamp):
-		sql = ''' INSERT INTO iskra_temperature(message_id, outside_temp, inside_temp, signature, timestamp)
-					VALUES(?,?,?,?,?)'''
-		try:
-			cur = self.conn.cursor()
-			result = cur.execute(sql, (str(message_id),outside_temp, inside_temp, str(signature), str(timestamp)))
-			self.conn.commit()
-			result = self.conn.commit()
-			# return cur.lastrowid
-		except Error as e:
-			print(e)
-		except Exception as e:
-			print("Exception in _query: {e}")	
-
-	def insert_emon_smartmeter(self, electricity_tariff, energy_delivered_tariff1, energy_delivered_tariff2, 
-							energy_returned_tariff1, energy_returned_tariff2, equipment_id, gas_device_type, 
-							gas_equipment_id, identification, p1_version, power_delivered, power_delivered_l1, 
-							power_returned, power_returned_l1, timestamp, signature):
-		sql = ''' INSERT INTO emon_smartmeter(electricity_tariff, energy_delivered_tariff1, energy_delivered_tariff2, 
-							energy_returned_tariff1, energy_returned_tariff2, equipment_id, gas_device_type, 
-							gas_equipment_id, identification, p1_version, power_delivered, power_delivered_l1, 
-							power_returned, power_returned_l1, timestamp, signature)
-			VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
-		try:
-			cur = self.conn.cursor()
-			result = cur.execute(sql, (str(electricity_tariff),
-										energy_delivered_tariff1,
-										energy_delivered_tariff2,
-										energy_returned_tariff1,
-										energy_returned_tariff2, 
-										str(equipment_id),
-										gas_device_type, 
-										str(gas_equipment_id),
-										str(identification),
-										str(p1_version),
-										power_delivered,
-										power_delivered_l1,
-										power_returned,
-										power_returned_l1,
-										str(timestamp),
-										str(signature)))
-			self.conn.commit()
-			result = self.conn.commit()
-			# return cur.lastrowid
-		except Error as e:
-			print(e)
-		except Exception as e:
-			print("Exception in _query: {e}")	
 
 	def setup(self):
 		if self.conn is not None:
@@ -151,9 +70,57 @@ class dbManager:
 		else:
 			print("Error! cannot create the database connection.")
 
+	def insert_iskra_energie(self, message_id, energymeter_name, watt, wH, total, signature, timestamp):
+		sql = '''INSERT INTO iskra_energie(message_id, energymeter_name, watt, wH, total, signature, timestamp)
+					VALUES(?,?,?,?,?,?,?)'''
+		print("Inserting data into iskra energie")
+		try:
+			cur = self.conn.cursor()
+			cur.execute(sql, (str(message_id), str(energymeter_name), watt, wH, total,
+						  str(signature), str(timestamp)))
+			self.conn.commit()
+		except Error as e:
+			print(e)
+		except Exception as e:
+			print("Exception in _query: {e}")
+
+	def insert_iskra_temperature(self, message_id, temperature, signature, timestamp):
+		sql = '''INSERT INTO iskra_temperature(message_id, temperature, signature, timestamp)
+					VALUES(?,?,?,?)'''
+		print("Inserting data into iskra temperature")
+		try:
+			cur = self.conn.cursor()
+			cur.execute(sql, (str(message_id),temperature, str(signature), str(timestamp)))
+			self.conn.commit()
+		except Error as e:
+			print(e)
+		except Exception as e:
+			print("Exception in _query: {e}")	
+
+	def insert_simon_energymeter(self, energymeter_name, watt, wH, total, signature, timestamp):
+		sql = '''INSERT INTO simon_meter(energymeter_name, watt, wH, total, signature, timestamp)
+							VALUES(?,?,?,?,?,?)'''
+		print("Inserting data into simon energymeter")
+		try:
+			cur = self.conn.cursor()
+			cur.execute(sql, str(energymeter_name), watt, wH, total, str(signature), str(timestamp))
+			self.conn.commit()
+		except Error as e:
+			print(e)
+		except Exception as e:
+			print("Exception in _query: {e}")
+
 	def retrieve_iskra_energie_last(self):
+			cur = self.conn.cursor()
+			cur.execute("SELECT * FROM iskra_energie ORDER BY id DESC LIMIT 1")
+			result = cur.fetchone()
+			print("Retrieving from iskra energy last row")
+			return result
+
+	def retrieve_simon_energie_last(self):
 		cur = self.conn.cursor()
-		cur.execute("SELECT * FROM iskra_energie ORDER BY id DESC LIMIT 1")
+		cur.execute("SELECT * FROM simon_meter ORDER BY id DESC LIMIT 1")
+		print("Retrieving from simon energy last row")
 		result = cur.fetchone()
 		return result
 
@@ -168,8 +135,6 @@ class dbManager:
 		sql = '''INSERT INTO overview(name, table_name)VALUES(\"Temperatuur Sensor\", \"iskra_temperature\")'''
 		print(sql)
 		self.insert(sql)
-
-
 
 	def insert(self, query):
 		try:
--- database/main.py
+++ database/main.py
@@ -1,108 +1,83 @@
-from dbmanager import dbManager
 import os
-import uuid
-import paho.mqtt.client as mqttclient
 import time
 import json
+
+import paho.mqtt.client as mqttclient
+
 from datetime import datetime
+from dbManager import dbManager
 
 connected_own = False
-connected_school = False
 
 def main():
-	print("Starting DB Manager!")
+    print("Starting DB and MQTT Manager!")
+    # Own smartmeter
+    broker_address = "iot.paulhobbel.me"
+    port = 1883
+    topic = "smartmeter/log"
 
+    client_own = mqttclient.Client("TIMDB_PATJON_BACKEND")
+    client_own.on_connect = on_connect
+    client_own.on_message = on_message
+    client_own.connect(broker_address, port=port)
+    client_own.loop_start()
 
-	
-	# Own smartmeter
-	broker_address="iot.paulhobbel.me"
-	port = 1883
-	topic = "smartmeter/log"
+    while connected_own != True:
+        time.sleep(0.1)
+        client_own.subscribe(topic, 1)
 
-	client_own = mqttclient.Client("TIMDB_PATJON_BACKEND")
-	client_own.on_connect= on_connect
-	client_own.on_message = on_message
-
-	client_own.connect(broker_address, port=port)
-	client_own.loop_start()
-
-	# School
-	port_school = 11883 
-	topic_school = "smartmeter/log"
-	username_school = "smartmeter_admin"
-	password_school = "s3_sm4rtm3t3r"
-	host_school = "sendlab.avansti.nl"
-	client_school = mqttclient.Client("TIMDB_PATJON_BACKEND_SCHOOl")
-	client_school.on_connect= on_connect_school
-	client_school.on_message = on_message_school
-	client_school.username_pw_set(username_school, password=password_school)
-	client_school.connect(host_school, port=port_school)
-	client_school.loop_start()
-
-	while connected_own != True and connected_school != True:
-		time.sleep(0.1)
-		client_own.subscribe(topic, 1)
-		client_school.subscribe(topic_school, 1)
-		
 
 def on_connect(client, userdata, flags, rc):
-	if rc == 0:
-		print("connected to broker of our own")
-		connected_own = True
-	else:
-		print("Connection failed")
+    if rc == 0:
+        print("connected to broker of our own")
+        connected_own = True
+    else:
+        print("Connection failed")
+
 
 def on_message(client, userdata, message):
-	db = dbManager(os.path.dirname(os.path.realpath(__file__)) + "/emonv3.db")
-	db.setup()
-	mes = str(message.payload.decode("utf-8"))
-	parsed_js = (json.loads(mes))
-	message_id = str((parsed_js['id']))
-	if message_id == "TIMDB_PATJON_EMON":
-		print("Received data from: TIMDB_PATJON_EMON")
-		result = db.retrieve_iskra_energie_last()
-		if result == None:
-			
-			energymeter_name = str((parsed_js['identification']))
-			watt = parsed_js['watt']
-			wattHour = watt / 12
-			totalkwh = wattHour / 1000
-			signature = str((parsed_js['signature']))
-			dateTimeObj = datetime.now()
-			timestamp = dateTimeObj.strftime("%d-%b-%Y %H:%M:%S")
-			db.insert_iskra_energie(message_id, energymeter_name, watt, wattHour,totalkwh, signature, timestamp)
-		else:
-			energymeter_name = str((parsed_js['identification']))
-			watt = parsed_js['watt']
-			wattHour = watt / 12
-			totalkwh = (wattHour / 1000) + result[5]
-			signature = str((parsed_js['signature']))
-			dateTimeObj = datetime.now()
-			timestamp = dateTimeObj.strftime("%d-%b-%Y %H:%M:%S")
+    db = dbManager(os.path.dirname(os.path.realpath(__file__)) + "/emonv3.db")
+    db.setup()
+    mes = str(message.payload.decode("utf-8"))
+    parsed_js = (json.loads(mes))
+    message_id = str((parsed_js['id']))
+    if message_id == "TIMDB_PATJON_EMON":
+        print("Received data from: TIMDB_PATJON_EMON")
+        result = db.retrieve_iskra_energie_last()
+        energymeter_name = str((parsed_js['identification']))
+        watt = parsed_js['watt']
+        signature = str((parsed_js['signature']))
+        dateTimeObj = datetime.now()
+        timestamp = dateTimeObj.strftime("%d-%b-%Y %H:%M:%S")
+        if result == None:
+            wattHour = watt / 30
+            totalkwh = wattHour / 1000
+            db.insert_iskra_energie(message_id, energymeter_name, watt, wattHour, totalkwh, signature, timestamp)
+        else:
+            wattHour = watt / 30
+            totalkwh = (wattHour / 1000) + result[5]
+            db.insert_iskra_energie(message_id, energymeter_name, watt, wattHour, totalkwh, signature, timestamp)
 
-			db.insert_iskra_energie(message_id, energymeter_name, watt, wattHour,totalkwh, signature, timestamp)
-			
-	if message_id == "TIMDB_PATJON_EMON_TEMP_SENSOR":
-		outside_temp = parsed_js['watt']
-		inside_temp = parsed_js['watt']
-		signature = str((parsed_js['signature']))
-		dateTimeObj = datetime.now()
-		timestamp = dateTimeObj.strftime("%d-%b-%Y %H:%M:%S")
-		db.insert_iskra_temperature(message_id, outside_temp, inside_temp, signature, timestamp)
+    if message_id == "TIMDB_PAJTON_EMON_TEMP":
+        print("Received data from: TIMDB_PATJON_EMON_TEMP_SENSOR")
+        temperature = parsed_js['temperature']
+        signature = str((parsed_js['signature']))
+        dateTimeObj = datetime.now()
+        timestamp = dateTimeObj.strftime("%d-%b-%Y %H:%M:%S")
+        db.insert_iskra_temperature(message_id, temperature, signature, timestamp)
 
+    # simon
 
-def on_connect_school(client, userdata, flags, rc):
-	if rc == 0:
-		print("connected to broker of school")
-		connected_school = True
-	else:
-		print("Connection failed")
-
-
-def on_message_school(client, userdata, message):
-	mes = str(message.payload.decode("utf-8"))
-	print(mes)
-
+    if str(parsed_js['signature']) == "2019-ETI-EMON-V01-SIM":
+        print("Received data from: 2019-ETI-EMON-V01-SIM")
+        result = db.retrieve_iskra_energie_last()
+        watt = parsed_js['w']
+        wattHour = watt / 12
+        kwH = (wattHour / 1000) + result[5]
+        signature = str(parsed_js['signature'])
+        dateTimeObj = datetime.now()
+        timestamp = dateTimeObj.strftime("%d-%b-%Y %H:%M:%S")
+        db.insert_simon_energymeter("ISKRA-MT382", watt, wattHour, kwH, signature, timestamp)
 
 if __name__ == "__main__":
-	main()
+    main()
