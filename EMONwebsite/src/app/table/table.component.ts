d8454e83a6f3ecf01958d60bd2f8cc257891f303
Unknown <timdebooij9@hotmail.com>
2020-03-22 04:22:37 +0100

    [ADDED] Front end v2 with overview and extra page, navbar en routing

--- EMONwebsite/src/app/table/table.component.ts
+++ EMONwebsite/src/app/table/table.component.ts
@@ -21,43 +21,8 @@ export class TableComponent implements OnInit {
   constructor(public client: HttpClient,) { }
 
   ngOnInit() {
-    this.getSensors();
     
   }
 
-  //Gets all the sensornames that are saved in the database and have an own table, first entrie is used to get the data. Later this list should be clickable by user and then get data
-  getSensors(){
-    this.value = this.client.get("http://localhost:8000/api/sensors").subscribe(val => {
-      this.sensorName = JSON.parse(JSON.stringify(val));
-      this.sensorName = this.sensorName["data"][0]["table_name"];
-      this.getData(this.sensorName);
-    });
-  }
-
-  //Gets all the data from sensor of the parameter: name. This data is saved in the FullMessage model for later showing
-  getData(name: string) {
-    console.log("send name: " + name);
-    this.value = this.client.get("http://localhost:8000/api/data/?sensor=" + name).subscribe(val => {
-      this.value = JSON.parse(JSON.stringify(val));
-      console.log("real data: " + this.value["data"][0]["watt"]);
-      console.log("real data: " + this.value["data"][0]["time"]);
-      console.log("real data: " + this.value["data"][0]["temperature"]);
-      console.log("real data: " + this.value["data"][0]["totalEnergy"]);
-      this.fullMessage.message = this.value["message"];
-      this.fullMessage.count = this.value["count"];
-      this.fullMessage.data = new Array<SensorData>();
-      for(var i=0; i<this.value["count"]-1; i++){
-        var sensData: SensorData = new SensorData();
-          sensData.id = this.value["data"][i]["id"];
-          sensData.time = this.value["data"][i]["time"];
-          sensData.temperature = this.value["data"][i]["temperature"];
-          sensData.humidity = this.value["data"][i]["humidity"];
-          sensData.watt = this.value["data"][i]["watt"];
-          sensData.totalEnergyUse = this.value["data"][i]["totalEnergy"];
-          sensData.returnedEnergy = this.value["data"][i]["returnedEnergy"];
-          this.fullMessage.data.push(sensData);
-      }
-  });
-  }
 
 }
